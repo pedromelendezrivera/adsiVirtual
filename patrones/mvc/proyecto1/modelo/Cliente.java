c0d75f53be405839f6ff6f97ced8f8c3f3e8e50b
Pedro Melendez <pmelendezrivera@mail.unicundi.edu.co>
2018-11-13 19:54:04 -0500

    Add files via upload

---
+++ patrones/mvc/proyecto1/modelo/Cliente.java
@@ -0,0 +1,34 @@
+package modelo;
+
+/**
+ *
+ * @author PMELENDEZ
+ */
+public class Cliente {
+	private int id;
+	private String nombre;
+	private String apellido;
+	
+	
+	public Cliente() {
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+}
