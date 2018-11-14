2f10547e193b230fa9a4d4f86212db04c5b34a29
Pedro Melendez <pmelendezrivera@mail.unicundi.edu.co>
2018-11-13 19:54:52 -0500

    Add files via upload

---
+++ patrones/mvc/proyecto1/vista/ClienteView.java
@@ -0,0 +1,14 @@
+package vista;
+
+/**
+ *
+ * @author PMELENDEZ
+ */
+public class ClienteView {
+	public void imprimirDatosCliente(int id,String nombre, String apellido) {
+		System.out.println("**** DATOS CLIENTE ****");
+		System.out.println("Id: "+id);
+		System.out.println("Nombre: "+nombre);
+		System.out.println("Apellido: "+apellido);
+	}
+}
