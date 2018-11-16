0a5680097fc398e721726af88b28555d2e2aea4d
Pedro Melendez <pmelendezrivera@mail.unicundi.edu.co>
2018-11-15 21:19:33 -0500

    Create ClienteVO.java

---
+++ patrones/mvc/proyecto2/modelo/ClienteVO.java
@@ -0,0 +1,43 @@
+package modelo;
+
+/**
+ *
+ * @author PMELENDEZ
+ */
+public class ClienteVO {
+	private int id;
+	private String nombre;
+	private String apellido;
+	
+	
+	public ClienteVO(int id, String nombre, String apellido) {
+		this.id = id;
+		this.nombre = nombre;
+		this.apellido = apellido;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+	@Override
+	public String toString() {
+		return this.getNombre()+" "+this.getApellido();
+	}
+}
