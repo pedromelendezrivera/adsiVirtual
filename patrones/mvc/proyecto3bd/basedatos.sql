652cff862a7e9bea5b31cecd778ce6b25c58ad02
Pedro Melendez <pmelendezrivera@mail.unicundi.edu.co>
2018-11-22 20:24:11 -0500

    Update basedatos.sql

--- patrones/mvc/proyecto3bd/basedatos.sql
+++ patrones/mvc/proyecto3bd/basedatos.sql
@@ -1,5 +1,4 @@
 
-
 CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
   `usuario` varchar(45) CHARACTER SET latin1 NOT NULL,
@@ -9,3 +8,8 @@ CREATE TABLE `usuarios` (
   `last_session` datetime DEFAULT '0000-00-00 00:00:00',
   `id_tipo` varchar(2) CHARACTER SET latin1 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `rol` (`id_tipo`);
+  
